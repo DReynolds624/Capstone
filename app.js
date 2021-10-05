70cea3555497c321c303d07c098afe8c081cb73a
DReynolds624 <87577834+DReynolds624@users.noreply.github.com>
2021-10-05 17:04:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log
+
+
+
+$.ajax({
+    url: "/data/data.json",
+    type: "GET",
+}).then(
+    (data) => {
+
+$.getJSON('./data.json', data -> {
+    console.log(data);
+    const container = 
+})
